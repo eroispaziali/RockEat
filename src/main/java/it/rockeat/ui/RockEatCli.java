f57386c22951f7313e0bdca3083366ecb47f6130
Lorenzo Frattini <lorenzo.frattini@gmail.com>
2012-07-23 14:53:21 +0200

    rimozione crawler

--- src/main/java/it/rockeat/ui/RockEatCli.java
+++ src/main/java/it/rockeat/ui/RockEatCli.java
@@ -6,8 +6,6 @@ import it.rockeat.exception.ConnectionException;
 import it.rockeat.exception.DownloadException;
 import it.rockeat.exception.FileSaveException;
 import it.rockeat.exception.ParsingException;
-import it.rockeat.http.ConnectionManager;
-import it.rockeat.http.RockCrawler;
 import it.rockeat.model.Album;
 import it.rockeat.model.RockitTrack;
 import it.rockeat.source.MusicSource;
@@ -28,17 +26,10 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import edu.uci.ics.crawler4j.crawler.CrawlConfig;
-import edu.uci.ics.crawler4j.crawler.CrawlController;
-import edu.uci.ics.crawler4j.fetcher.PageFetcher;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
-
 public class RockEatCli {
 		 
 		private static final String DISABLE_TAGGING = "t";
 		private static final String URL = "u";
-		private static final String CRAWLING = "x";
 		private static final String EAT = "e";
 		private static final String TESTING = "c";
 		private static PrintStream out;
@@ -56,25 +47,6 @@ public class RockEatCli {
 	    	formatter.printHelp( "java -jar rockeat.jar" , options );
 		}
 		
-		public static void crawl(String url) throws Exception {
-			   String crawlStorageFolder = "RockEat-tmp";
-		       int numberOfCrawlers = 1;
-		       CrawlConfig config = new CrawlConfig();
-		       config.setCrawlStorageFolder(crawlStorageFolder);
-		       config.setUserAgentString(ConnectionManager.USER_AGENT_STRING);
-		       config.setPolitenessDelay(10);
-		       config.setMaxDepthOfCrawling(7);
-		       config.setMaxPagesToFetch(10000);
-		       config.setResumableCrawling(false);
-		       PageFetcher pageFetcher = new PageFetcher(config);
-		       RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-		       RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-		       CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-		       controller.addSeed(url);
-		       out.println();
-		       controller.start(RockCrawler.class, numberOfCrawlers);
-		}
-		
 		public static void main(String[] args) {
 			
 			try {
@@ -87,7 +59,6 @@ public class RockEatCli {
 			options.addOption(new Option(URL, "url", true, "specifica l'indirizzo della pagina da analizzare"));
 			options.addOption(new Option(DISABLE_TAGGING, "disable-tagging", false, "disabilita la scrittura automatica dei tag ID3"));
 			options.addOption(new Option(EAT, "eat", false, "mangia tutte le tracce disponibili"));
-			options.addOption(new Option(CRAWLING, "explore", false, "esplora le pagine vicine (funzionalità sperimentale)"));
 			options.addOption(new Option(TESTING, "test", false, "verifica di essere in grado di scaricare"));
 			
 		    try {
@@ -100,70 +71,61 @@ public class RockEatCli {
 						String url = commandLine.getOptionValue(URL);
 						MusicSource eater = controller.tuneInSource(url);
 						
-						if (commandLine.hasOption(CRAWLING)) {
+								
+						if (commandLine.hasOption(TESTING)) {
 							out.println(Messages.PARSE_IN_PROGRESS);
-							try {
-								crawl(url);
-							} catch (Exception e) {
-								out.println(Messages.ERROR_CRAWLING);
+							
+							Boolean testResult = eater.runTest();
+							if (BooleanUtils.isFalse(testResult)) {
+								out.println(Messages.TEST_ERROR);
+							} else {
+								out.println(Messages.TEST_SUCCESS);
 							}
 						} else {
-								
-							if (commandLine.hasOption(TESTING)) {
-								out.println(Messages.PARSE_IN_PROGRESS);
-								
-								Boolean testResult = eater.runTest();
-								if (BooleanUtils.isFalse(testResult)) {
-									out.println(Messages.TEST_ERROR);
-								} else {
-									out.println(Messages.TEST_SUCCESS);
-								}
-							} else {
-								out.println(Messages.PARSE_IN_PROGRESS);
-								Album album = controller.findAlbum(url);
-								out.println(FormatUtils.formatAlbumData(album));
-								
-								if (commandLine.hasOption(EAT)) {
-									Integer progress = 0;
-									Integer count = album.getTracksCount();
-									for (RockitTrack track : album.getTracks()) {
-										progress++;
-										String st = StringUtils.leftPad(">", progress+1, "=") + StringUtils.leftPad("", count-progress, " ");
-										out.print("Download: [" + st + "] "+Long.toString(controller.getDownloadedTracks()+1) + "/" + Integer.toString(count));
-										out.print("\r");
-										try {
-											controller.download(album, track);
-										} catch (DownloadException e) {
-											/* Silently ignore */
-										} catch (FileSaveException e) {
-											/* Silently ignore */
-										}
-									}
-									String label = StringUtils.EMPTY;
-									if (controller.getDownloadedTracks()>0) {
-										label = Messages.DOWNLOAD_COMPLETE;
-										label = StringUtils.replace(label,"{0}", Long.toString(controller.getDownloadedTracks()));
-										label = StringUtils.replace(label,"{1}", FileUtils.byteCountToDisplaySize(controller.getBytesDownloaded()));
-
-									} else {
-										label = Messages.ERROR_DOWNLOAD;
-									}
-									out.println("\n" + label);
-									
-									/* Self-diagnostic test on failure */
-									if (controller.getDownloadedTracks()==0) {
-										Boolean testResult = eater.runTest();
-										if (BooleanUtils.isFalse(testResult)) {
-											out.println(Messages.TEST_ERROR);
-										} else {
-											out.println(Messages.TEST_REASON_UNKNOWN);
-										}
-									}
-								}
-									
+							out.println(Messages.PARSE_IN_PROGRESS);
+							Album album = controller.findAlbum(url);
+							out.println(FormatUtils.formatAlbumData(album));
 							
-							} 
-						}
+							if (commandLine.hasOption(EAT)) {
+								Integer progress = 0;
+								Integer count = album.getTracksCount();
+								for (RockitTrack track : album.getTracks()) {
+									progress++;
+									String st = StringUtils.leftPad(">", progress+1, "=") + StringUtils.leftPad("", count-progress, " ");
+									out.print("Download: [" + st + "] "+Long.toString(controller.getDownloadedTracks()+1) + "/" + Integer.toString(count));
+									out.print("\r");
+									try {
+										controller.download(album, track);
+									} catch (DownloadException e) {
+										/* Silently ignore */
+									} catch (FileSaveException e) {
+										/* Silently ignore */
+									}
+								}
+								String label = StringUtils.EMPTY;
+								if (controller.getDownloadedTracks()>0) {
+									label = Messages.DOWNLOAD_COMPLETE;
+									label = StringUtils.replace(label,"{0}", Long.toString(controller.getDownloadedTracks()));
+									label = StringUtils.replace(label,"{1}", FileUtils.byteCountToDisplaySize(controller.getBytesDownloaded()));
+
+								} else {
+									label = Messages.ERROR_DOWNLOAD;
+								}
+								out.println("\n" + label);
+								
+								/* Self-diagnostic test on failure */
+								if (controller.getDownloadedTracks()==0) {
+									Boolean testResult = eater.runTest();
+									if (BooleanUtils.isFalse(testResult)) {
+										out.println(Messages.TEST_ERROR);
+									} else {
+										out.println(Messages.TEST_REASON_UNKNOWN);
+									}
+								}
+							}
+								
+						
+						} 
 			        }
 					
 				} else {
